6e2536207b3944d26f05d1b89e229c4138eb63d8
racullen403 <racullen403@hotmail.co.uk>
2022-09-05 15:52:45 +0100

    Update

--- Python/DSA/_01_DSA/_12_AVLTree.py
+++ Python/DSA/_01_DSA/_12_AVLTree.py
@@ -2,8 +2,8 @@
 AVL Tree:
 
     This is a self-balancing BST where the balance factor at each node is -1, 0, or 1.
-        - The balance factor is simply the difference in height of the left and right subtrees.
-
+        - The balance factor is simply the difference in height of the left and right subtrees (we use -1 height if
+        a subtree does not exist).
 
     The self-balancing is achieved through using Left and Right Node rotations.
 
@@ -17,8 +17,9 @@ AVL Tree:
                                 /   \    /   \
                               b1    b2  c1   c2
 
-            In a Left-Rotation, we can think of it as pulling the left subtree, B, downwards, resulting in the right
-            subtree, C, being pulled up. We would then attach the left child of C onto the right child of A
+            In a Left-Rotation, we can think of it as pulling the root, A, down to the left, this results in the right
+            subtree, C, being pulled up, and placed as the root. We would then attach the left child of C onto the
+            right child of A.
 
                                       C
                                     /   \
@@ -28,8 +29,9 @@ AVL Tree:
                             /   \
                           b1    b2
 
-            In a Right-Rotation, we can think of it as pulling the right subtree, C, downwards, which will then pull
-            the left subtree, B, upwards. Finally, attach the right child of B on the left child position of A.
+            In a Right-Rotation, we can think of it as pulling the root, A, downwards to the right, this will then pull
+            the left subtree, B, upwards into the root position. Finally, attach the right child of B on the left child
+            position of A.
 
                                       B
                                     /   \
@@ -57,15 +59,17 @@ AVL Tree:
         This will mean the balance factor of A is now +2, and Y was inserted at the end of B's left or right subtrees,
         b1 or b2.
 
-        Note, when A becomes unbalanced, b1 and b2 will always be nodes of the same initial height before insertion, or
-        else neither will exist and Y would be inserted straight onto B, causing the imbalance. This is simply how a
-        balanced tree becomes unbalanced, you will never find a case where b1 and b2 don't have the same initial
-        height, yet the tree is unbalanced at A after an insertion into B.
+        Note, when A becomes unbalanced after insertion into B, b1 and b2 will always be nodes of the same initial
+        height before insertion, or else neither will exist and Y would be inserted straight onto B, causing the
+        imbalance. If b1 and b2 where different heights, then insertion into one of them would either unbalance
+        B (and A, but we consider B first) or change to balance factor at B back to 0.
+
+        We consider the insertion of Y into subtree B, cause a +2 bf in A.
 
 
         Case 1: Y was inserted into b1
 
-            We simply do a right rotation between A and B
+            We simply do a right rotation on A
 
                                  B
                                 / \
@@ -74,17 +78,18 @@ AVL Tree:
                                  b2  a1
 
             We reduced the left subtree height by 1 and increased the right subtree height by one, resulting in B
-            having balance factor 0.
+            having balance factor 0 at B.
 
-            We know the height(b2) = height(b1) - 1 and height(a1) = height(b1)-1 and so all nodes are balanced.
+            We know after insertion of Y, the height(b2) = height(b1) - 1 and height(a1) = height(b1)-1 and so
+            all nodes are balanced.
 
 
         Case 2: Y was inserted into b2
 
             If we just did a right rotation like above, height(b1) = height(b2) - 1, this would mean the right subtree
-            of B above now have a balance factor of -2.
+            of B in the above case would now have a balance factor of -2.
 
-            We correct this by first doing a left rotation between b2 and B in the initial tree.
+            We correct this by first doing a left rotation on B in the initial tree.
 
                                  A
                                 / \
@@ -94,7 +99,7 @@ AVL Tree:
                             / \
                            b1  b2_l
 
-            Now we do the right rotation between b2 and A.
+            Now we do the right rotation between on A.
 
                                      b2
                                 /         \
@@ -115,8 +120,13 @@ AVL Tree:
 
 
     Deletion:
-        - Deletion occurs in the same way it does in a BST, however, after we remove the node, we must re-balance
-        the tree like in insertion
+
+        Deletion occurs in the same way it does in a BST, however, after we remove the node, we must re-balance
+        the tree like in insertion.
+
+            - We only every delete from a leaf node, or an internal node with only 1 child. In all other cases, we
+            would find the successor and replace the internal node with it, then continue as before but try to remove
+            the successor.
 
 """
 
@@ -127,6 +137,7 @@ class Node:
         self.val = val
         self.left = None
         self.right = None
+        self.parent = None
         self.height = 0
 
 
@@ -135,16 +146,18 @@ class AVL:
     def __init__(self):
         self.root = None
 
+    # Recursive insert
     def insert(self, val):
-
         def insert_r(node, value):
             if node is None:
                 return Node(value)
             else:
                 if value <= node.val:
                     node.left = insert_r(node.left, value)
+                    node.left.parent = node
                 else:
                     node.right = insert_r(node.right, value)
+                    node.right.parent = node
 
                 node.height = 1 + max(self.get_height_helper(node.left), self.get_height_helper(node.right))
 
@@ -163,8 +176,142 @@ class AVL:
                         node.right = self.right_rotate(node.right)
                         return self.left_rotate(node)
                 return node
-
         self.root = insert_r(self.root, val)
+
+    # None recursive insert using parent attribute
+    def insert_non_recursive(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            temp = self.root
+            inserted = False
+            while not inserted:  # Insert Value and end with temp pointing to inserted node
+                if val <= temp.val:
+                    if not temp.left:
+                        temp.left = Node(val)
+                        temp.left.parent = temp
+                        inserted = True
+                    temp = temp.left
+                else:
+                    if not temp.right:
+                        temp.right = Node(val)
+                        temp.right.parent = temp
+                        inserted = True
+                    temp = temp.right
+            while temp:  # Correct height and ensure nodes are balanced as we return up the tree to the root
+                temp.height = 1 + max(self.get_height_helper(temp.left), self.get_height_helper(temp.right))
+                bf = self.get_height_helper(temp.left) - self.get_height_helper(temp.right)
+                if bf > 1:
+                    if val <= temp.left.val:
+                        self.right_rotate(temp)
+                    else:
+                        self.left_rotate(temp.left)
+                        self.right_rotate(temp)
+                elif bf < -1:
+                    if val > temp.right.val:
+                        self.left_rotate(temp)
+                    else:
+                        self.right_rotate(temp.right)
+                        self.left_rotate(temp)
+                temp = temp.parent
+
+    def delete(self, val):
+        def delete_r(node, value):
+            if node is None:
+                return node
+            else:
+                if value < node.val:
+                    node.left = delete_r(node.left, value)
+                    if node.left:
+                        node.left.parent = node
+                elif value > node.val:
+                    node.right = delete_r(node.right, value)
+                    if node.right:
+                        node.right.parent = node
+                else:
+                    if not node.left:
+                        return node.right
+                    elif not node.right:
+                        return node.left
+                    else:
+                        inorder = self.inorder_successor(node.right)
+                        node.val = inorder.val
+                        node.right = delete_r(node.right, inorder.val)
+
+                node.height = 1 + max(self.get_height_helper(node.left), self.get_height_helper(node.right))
+                balance_factor = self.get_height_helper(node.left) - self.get_height_helper(node.right)
+
+                if balance_factor > 1:
+                    if value <= node.left.val:
+                        self.right_rotate(node)
+                        return node.parent
+                    else:
+                        self.left_rotate(node.left)
+                        self.right_rotate(node)
+                        return node.parent
+                elif balance_factor < -1:
+                    if val > node.right.val:
+                        self.left_rotate(node)
+                        return node.parent
+                    else:
+                        self.right_rotate(node.right)
+                        self.left_rotate(node)
+                        return node.parent
+                else:
+                    return node
+        self.root = delete_r(self.root, val)
+
+    def delete_non_recursive(self, val):
+        if not self.root:
+            return
+        else:
+            temp = self.root
+            deleted = False
+            while temp and not deleted:
+                if val < temp.val:
+                    temp = temp.left
+                elif val > temp.val:
+                    temp = temp.right
+                else:  # Found val
+                    if not temp.right or not temp.left:  # One or None children
+                        if temp.right:
+                            successor = temp.right
+                        else:
+                            successor = temp.left
+                        if not temp.parent:
+                            self.root = successor
+                        elif temp == temp.parent.left:
+                            temp.parent.left = successor
+                        else:
+                            temp.parent.right = successor
+                        if successor:
+                            successor.parent = temp.parent
+                        temp = temp.parent
+                        deleted = True
+                    else:  # Two children
+                        successor = self.inorder_successor(temp.right)
+                        temp.val = successor.val
+                        val = successor.val
+                        temp = temp.right
+            if not temp:  # Couldn't find val
+                return
+            else:
+                while temp:  # Correct height and ensure nodes are balanced as we return up the tree to the root
+                    temp.height = 1 + max(self.get_height_helper(temp.left), self.get_height_helper(temp.right))
+                    bf = self.get_height_helper(temp.left) - self.get_height_helper(temp.right)
+                    if bf > 1:
+                        if val <= temp.left.val:
+                            self.right_rotate(temp)
+                        else:
+                            self.left_rotate(temp.left)
+                            self.right_rotate(temp)
+                    elif bf < -1:
+                        if val > temp.right.val:
+                            self.left_rotate(temp)
+                        else:
+                            self.right_rotate(temp.right)
+                            self.left_rotate(temp)
+                    temp = temp.parent
 
     def get_height_helper(self, node):
         if node is None:
@@ -172,22 +319,49 @@ class AVL:
         return node.height
 
     def right_rotate(self, node):
+        root = node.parent
         B = node.left
         b2 = B.right
-        node.left = b2
         B.right = node
+        node.parent = B
+        node.left = b2
+        if b2:
+            b2.parent = node
+        if not root:
+            self.root = B
+        elif node == root.left:
+            root.left = B
+        else:
+            root.right = B
+        B.parent = root
         node.height = 1 + max(self.get_height_helper(node.left), self.get_height_helper(node.right))  # Must do first
         B.height = 1 + max(self.get_height_helper(B.left), self.get_height_helper(B.right))
-        return B
+        return B  # Return B for the recursive case
 
     def left_rotate(self, node):
+        root = node.parent
         B = node.right
         b1 = B.left
         B.left = node
+        node.parent = B
         node.right = b1
+        if b1:
+            b1.parent = node
+        if not root:
+            self.root = B
+        elif root.left == node:
+            root.left = B
+        else:
+            root.right = B
+        B.parent = root
         node.height = 1 + max(self.get_height_helper(node.left), self.get_height_helper(node.right))  # Must do first
         B.height = 1 + max(self.get_height_helper(B.left), self.get_height_helper(B.right))
-        return B
+        return B  # Return B for the recursive case
+
+    def inorder_successor(self, node):
+        while node.left:
+            node = node.left
+        return node
 
     def show(self):
         def show_tree(node, count, left):
@@ -214,6 +388,7 @@ class AVL:
         show_tree(self.root, 0, True)
 
 
+# Example using the recursive insert
 def example1():
     tree = AVL()
     for i in range(10):
@@ -222,6 +397,37 @@ def example1():
         tree.show()
 
 
-example1()
+# Example using the non-recursive insert
+def example2():
+    tree = AVL()
+    for i in range(10):
+        print("\nInserting:", i, "\n")
+        tree.insert_non_recursive(i)
+        tree.show()
 
+
+# Example using the non-recursive insert + delete
+def example3():
+    tree = AVL()
+    for i in range(10):
+        tree.insert_non_recursive(i)
+    print("\n-----Initial Tree-----\n")
+    tree.show()
+    d = 0
+    tree.delete_non_recursive(d)
+    print("\n-----Delete {}-----".format(d))
+    tree.show()
+
+
+# Example using the recursive insert + delete
+def example4():
+    tree = AVL()
+    for i in range(10):
+        tree.insert(i)
+    print("\n-----Initial Tree-----\n")
+    tree.show()
+    d = 0
+    tree.delete(d)
+    print("\n-----Delete {}-----".format(d))
+    tree.show()
 
