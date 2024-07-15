a13e6c53d50e738437d80c266fc305564f572104
racullen403 <racullen403@hotmail.co.uk>
2024-07-15 23:34:12 +0100

    Update

---
+++ NeetCode150/Trees/SubtreeOfBinaryTree.py
@@ -0,0 +1,36 @@
+""" 
+Easy: Given 2 binary trees, return true/false if subRoot is a subtree of root
+
+Sol:
+    - We just use our Same Binary Tree solution to make the comparison, and then recursively call this as a helper function as we dfs through root.
+"""
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val 
+        self.left = left
+        self.right = right 
+
+
+def isSubtree(root, subRoot):
+
+    def sameTree(p, q):
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return sameTree(p.left, q.left) and sameTree(p.right, q.right) 
+        return False
+    
+    if not subRoot:
+        return True
+    if not root:
+        return False
+    if sameTree(root, subRoot):
+        return True
+    else:
+        return isSubtree(root.left, subRoot) or isSubtree(root.right, subTree)
+
+    
+        
+        
