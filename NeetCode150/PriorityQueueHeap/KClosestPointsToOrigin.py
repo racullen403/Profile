a5120b61f0a122c7f4e625260feb988755fb66ac
racullen403 <racullen403@hotmail.co.uk>
2024-07-19 23:22:44 +0100

    Update

---
+++ NeetCode150/PriorityQueueHeap/KClosestPointsToOrigin.py
@@ -0,0 +1,26 @@
+"""
+Medium: Given 2D array, points for the (x, y) plane, and integer k, return the k closest points to the origin (0, 0).
+
+Sol:
+    - Create a min heap with the distances for each point, map these points to the coordinates to return.
+"""
+import heapq
+import math
+
+def kClosest(points, k):
+    heap = [] 
+    lookup = {}
+    for x, y in points:
+        dist = math.sqrt(x**2 + y**2)
+        if dist not in lookup:
+            lookup[dist] = [] 
+        lookup[dist].append((x,y))
+        heapq.heappush(heap, dist)
+    res = [] 
+    while heap and k > 0:
+        dist = heapq.heappop(heap)
+        res.append(lookup[dist].pop())
+        k -= 1
+    return res
+
+    
